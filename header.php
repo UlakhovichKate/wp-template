9ea85f8262ea947fca813a709b9df7a356157b96
Ulakhovich Kate <e.ulahovich@belitsoft.com>
2018-09-24 15:59:09 +0300

    wp-site test

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title><?php wp_title('«', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<div class="wrapper">
+	<header>
+		<a href="/"><img src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="Whitesquare logo"></a>
+    <?php get_search_form(); ?>
+
+	</header>
+  <nav class="main-navigation">
+  <?php wp_nav_menu(array('menu' => 'General', 'menu_class' => 'top-menu')); ?>
+  </nav>
